288942908facaf751181471a147556a497041947
bonfimalan <alanbonfim2000@gmail.com>
2025-08-15 00:06:38 -0300

    fix: routes had the same names

--- gateway/install-gateway.sh
+++ gateway/install-gateway.sh
@@ -1,3 +1,3 @@
 kubectl apply -f gateway-manifest.yaml 
 # TODO check if need to wait for the gateway
-kubectl apply -f cloud-routes.yaml  dev-routes.yaml 
+kubectl apply -f cloud-routes.yaml -f dev-routes.yaml 
